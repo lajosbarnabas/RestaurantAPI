b761fc9bb2dbbd5fb634389ddae04e5351bf4e54
lajosbarnabas0 <lajosbarnabas0@gmail.com>
2024-12-09 12:44:36 +0100

    more tests

--- __tests__/restaurant.controller.test.js
+++ __tests__/restaurant.controller.test.js
@@ -1,14 +1,30 @@
 const restaurantController = require('../controllers/restaurant.controller')
 const restaurantModel = require('../models/restaurantModel')
+const httpMocks = require('node-mocks-http')
+const restaurantList = require('./mock-data/allRestaurants.json')
 
 restaurantModel.find = jest.fn()
+let req, rest, next
+
+beforeEach(() =>{
+    req = httpMocks.createRequest()
+    res = httpMocks.createResponse()
+    next = null
+})
 
 describe('A getAll végponthoz tartozó metódus tesztelése', () =>{
     it('A getAllRestaurants függvénynek léteznie kellene', () =>{
         expect(typeof restaurantController.getAllRestaurant).toBe('function')
     })
     it('A getAllRestaurant függvényben meg kellene hívni a model find() függvényét', () =>{
-        restaurantController.getAllRestaurant()
+        restaurantController.getAllRestaurant(req, res, next)
         expect(restaurantModel.find).toHaveBeenCalled()
+    })
+    it('A getAllRestaurant függvény vissza kellene adjon JSON listát az összes étteremmel, és 200-as státusz kódot', async () =>{
+        restaurantModel.find.mockReturnValue(restaurantList)
+        await restaurantController.getAllRestaurant(res, res, next)
+        expect(res.statusCode).toBe(200);
+        expect(res._isEndCalled()).toBeTruthy()
+        expect(res._getJSONData()).toStrictEqual(restaurantList)
     })
 })
--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,7 +1,8 @@
 const restaurantModel = require('../models/restaurantModel')
 
-const getAllRestaurant = () =>{
-    const ettermek = restaurantModel.find() // a find függvényt nem mi írtuk(külső függőség), ezért mockolni kell
+const getAllRestaurant = async (req, res, next) =>{
+    const ettermek = await restaurantModel.find() // a find függvényt nem mi írtuk(külső függőség), ezért mockolni kell
+    res.status(200).json(restaurantModel.find())
 }
 
 module.exports = {
