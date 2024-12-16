357f7f1339ae15d88fea5d04a623389b0ba738be
lajosbarnabas <lajos.barnabas@students.jedlik.eu>
2024-12-16 20:20:20 +0100

    delete függvényt létrehozása controllerben

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,11 +1,11 @@
 const restaurantModel = require('../models/restaurantModel')
 
-const getAllRestaurant = async (req, res, next) =>{
+const getAllRestaurant = async (req, res, next) => {
     const ettermek = await restaurantModel.find() // a find függvényt nem mi írtuk(külső függőség), ezért mockolni kell
     res.status(200).json(restaurantModel.find())
 }
 
-const createRestaurant = async (req, res) =>{
+const createRestaurant = async (req, res, next) => {
     const newRestaurant = await restaurantModel.create(req.body)
     res.status(200).json(restaurantModel.create())
 }
@@ -13,6 +13,11 @@ const createRestaurant = async (req, res) =>{
 const getRestaurantById = async (req, res, next) => {
     const restaurantById = await restaurantModel.findById(req.params.restaurant_id)
     res.status(200).json(restaurantById)
+}
+
+const deleteRestaurant = async (req, res, next) => {
+    const deleted = await restaurantModel.findByIdAndDelete(req.params.restaurant_id)
+    res.status(200).json(deleted)
 }
 
 
